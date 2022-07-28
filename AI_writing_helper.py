f2e2a1fcf8813ee06a3611f6c2101e4b634568e1
guohuan78 <781213930@qq.com>
2022-07-28 16:07:07 +0800

    AI写作外挂第一次提交

---
+++ AI_writing_helper.py
@@ -0,0 +1,369 @@
+import requests
+import json
+import webbrowser
+import os
+
+from qgui import CreateQGUI,MessageBox
+
+from qgui.notebook_tools import InputBox, BaseButton, HorizontalToolsCombine,CheckButton,BaseButton,RadioButton
+from qgui.manager import QStyle
+
+# 创建主界面
+q_gui = CreateQGUI(title="AI写作外挂",
+                   tab_names=["Key","续写","病句","作文","摘要","古诗","对联","改写","词语","押韵","组词"],
+                   style=QStyle.lumen)
+
+q_gui.set_navigation_about(author="郭睆（huàn）",
+                           version="1.2.0",
+                           github_url="https://github.com/guohuan78",
+                           # 根据我自己的需求添加了以下两个链接，这两个链接的实现更改了 qgui 的内容
+                           # 利用转到定义的功能可以找到，更改了下面两个文件
+                           # 更改 qgui/factory.py 107 行 和 121 行
+                           # 更改 qgui/base_frame.py 61 行 和 86 行
+                           # 上面有更改的文件我放在源码的对应路径了，可以直接替换
+                           # 如若不想更改 qgui，也可直接把下面两行注释掉，不影响程序功能，注意删除此文件 18 行末的逗号
+                           bilibili_url="https://space.bilibili.com/518491096?spm_id_from=333.1007.0.0",
+                           blog_url="https://guohuan78.github.io/"
+                        )
+
+q_gui.set_navigation_info(title="使用方式", info="右侧文本框中填写\n标签页可选择功能")
+
+q_gui.set_navigation_info(title="注意事项", info='''
+1.先填写Key才可使用
+2.有时请求返回需要较长时间，请耐心等待
+3.如遇错误，先检查是否正确填写Key，再次请求可能成功，仍错误可联系作者
+4.每个账号的Key单日调用上限为200次，总调用上限为2000次，此限制为所调用的文心大模型本身的限制（它也太扣了点）作者也无能为力。
+5.此软件免费，欢迎赞赏
+''')
+
+# 复用函数
+def getToken(args: dict):
+    # 获取Token
+    token = requests.request("POST",
+                            "https://wenxin.baidu.com/younger/portal/api/oauth/token",
+                            data={"grant_type": "client_credentials",
+                                "client_id": args["API Key"].get(),  # 填写你的API Key
+                                "client_secret": args["Secret Key"].get()},  # 填写你的Secret Key
+                            timeout=3)
+    return json.loads(token.text)["data"]
+
+def getPost(payload: dict):
+    print("开始请求")
+    response = requests.request("POST",
+                                "https://wenxin.baidu.com/younger/portal/api/rest/1.0/ernie/3.0/zeus",
+                                data=payload)
+
+    print(json.loads(response.text)["data"]["result"])
+    return json.loads(response.text)["data"]["result"]
+
+# 运行按钮对应函数
+def click_run_custom(args: dict):
+    payload={
+    'text': args["续写输入框"].get(),
+    'seq_len': args["续写输入框最多字数"].get(),
+    'task_prompt': '',
+    'dataset_prompt': '',
+    'access_token': getToken(args),
+    'topk': 10,
+    'stop_token': '',
+    'is_unidirectional': 1,
+    'min_dec_len':args["续写输入框最少字数"].get()
+    }
+    getPost(payload)
+
+def click_run_correction(args: dict):
+    payload={
+    'text': '改正下面文本中的错误：\"' + args["病句输入框"].get() + '\"',
+    'seq_len': 64,
+    'task_prompt': 'Correction',
+    'dataset_prompt': '',
+    'access_token': getToken(args),
+    'topk': 1,
+    'stop_token': '',
+    }
+    getPost(payload)
+
+def click_run_zuowen(args: dict):
+    payload={
+    'text': '作文题目：' + args["作文输入框"].get() + '\n体裁：' + args["RadioButton"].get() + '\n内容：',
+    'seq_len': args["作文输入框最多字数"].get(),
+    'task_prompt': '',
+    'dataset_prompt': 'zuowen',
+    'temperature': '1.0',
+    'penalty_score': '1.2',
+    'is_unidirectional': 0,
+    'min_dec_len': args["作文输入框最少字数"].get(),
+    'min_dec_penalty_text': '[gEND]',
+    'access_token': getToken(args),
+    'topk': 1,
+    }
+    getPost(payload)
+
+def click_run_summarization(args: dict):
+    payload={
+    'text': '请给下面这段话写一句摘要：“' + args["摘要输入框"].get() + '”',
+    'seq_len': 32,
+    'task_prompt': 'Summarization',
+    'dataset_prompt': '',
+    'topk': 1,
+    'access_token': getToken(args)
+    }
+    getPost(payload)
+
+def click_run_poetry(args: dict):
+    payload={
+    'text': '古诗续写：' + args["古诗输入框"].get(),
+    'seq_len': args["古诗输入框最多字数"].get(),
+    'min_dec_len': args["古诗输入框最少字数"].get(),
+    'task_prompt': '',
+    'dataset_prompt': 'poetry',
+    'topk': 10,
+    'stop_token': '',
+    'is_unidirectional': 1,
+    'access_token': getToken(args)
+    }
+    getPost(payload)
+
+def click_run_couplet(args: dict):
+    payload={
+    'text': '上联:' + args["对联输入框"].get() + ' 下联:',
+    'seq_len': 32,
+    'task_prompt': '',
+    'dataset_prompt': 'couplet',
+    'topk': 10,
+    'stop_token': '',
+    'access_token': getToken(args)
+    }
+    getPost(payload)
+
+def click_run_rewrite(args: dict):
+    payload={
+    'text': '“' + args["改写输入框"].get() + '”换种表达，意思不变：',
+    'seq_len': 64,
+    'task_prompt': 'Paraphrasing',
+    'dataset_prompt': '',
+    'topk': 1,
+    'stop_token': '',
+    'access_token': getToken(args)
+    }
+    getPost(payload)
+
+def click_run_words(args: dict):
+    synonym = ''
+    antonym = ''
+    interpretation = ''
+    if args["CheckButton-近义词"].get() == '1':
+        print("近义词 ",end = '')
+        payload={
+        'text': '“' + args["词语输入框"].get() + '”的近义词是：“[MASK]”',
+        'seq_len': 16,
+        'task_prompt': '',
+        'dataset_prompt': '',
+        'topk': 1,
+        'stop_token': '',
+        'access_token': getToken(args)
+        }
+        synonym = getPost(payload)
+    if args["CheckButton-反义词"].get() == '1':
+        print("反义词 ",end = '')
+        payload={
+        'text': '“' + args["词语输入框"].get() + '”的反义词是：“[MASK]”',
+        'seq_len': 16,
+        'task_prompt': '',
+        'dataset_prompt': '',
+        'topk': 1,
+        'stop_token': '',
+        'access_token': getToken(args)
+        }
+        antonym = getPost(payload)
+    if args["CheckButton-释义"].get() == '1':
+        print("释义 ",end = '')
+        payload={
+        'text': '“' + args["词语输入框"].get() + '”的释义是：“[MASK]”',
+        'seq_len': 16,
+        'task_prompt': '',
+        'dataset_prompt': '',
+        'topk': 1,
+        'stop_token': '',
+        'access_token': getToken(args)
+        }
+        interpretation = getPost(payload)
+    if args["CheckButton-近义词"].get() == '1':
+        print("\n近义词：" + synonym)
+    if args["CheckButton-反义词"].get() == '1':
+        print("\n反义词：" + antonym)
+    if args["CheckButton-释义"].get() == '1':
+        print("\n释义：" + interpretation)
+    if args["CheckButton-近义词"].get() == '0' and args["CheckButton-反义词"].get() == '0' and args["CheckButton-释义"].get() == '0':
+        print("您没有选中任何一个需要的功能，请点击需要的功能前的方框。")
+
+def click_run_rhyme(args: dict):
+    payload={
+    'text': '问题：与“' + args["押韵输入框"].get() + '”字押韵的字有哪些？回答：很多，比如：',
+    'seq_len': 64,
+    'min_dec_len': 1,
+    'task_prompt': '',
+    'dataset_prompt': '',
+    'topk': 1,
+    'stop_token': '',
+    'access_token': getToken(args)
+    }
+    getPost(payload)
+
+def click_run_rhyme_words(args: dict):
+    payload={
+    'text': '问题：含“' + args["组词输入框"].get() + '”字的词语有哪些？回答：',
+    'seq_len': 64,
+    'min_dec_len': 1,
+    'task_prompt': '',
+    'dataset_prompt': '',
+    'topk': 1,
+    'stop_token': '',
+    'access_token': getToken(args)
+    }
+    getPost(payload)
+
+def yanggu_callback(event):
+    webbrowser.open_new("https://wenxin.baidu.com/younger/apiDetail?id=20006")
+
+def wechatpay_callback(event):
+    q_gui.print_image("images/wechatpay.png")
+
+def key_save(args: dict):
+    key = {
+        "API Key": args["API Key"].get(),
+        "Secret Key": args["Secret Key"].get()
+    }
+    key = json.dumps(key)
+    f = open('key.json', 'w')
+    f.write(key)
+    f.close()
+    print("保存成功！")
+
+def get_API_Key():
+    if os.path.exists('key.json'):
+        f = open('key.json', 'r')
+        content = f.read()
+        key = json.loads(content)
+        return key['API Key']
+    else:
+        MessageBox.info('''
+本软件依赖文心大模型，需要申请 key 并填写方可使用，申请免费。
+将自动为您打开申请网站，请到网站最下方调用专区点击“申请调用”，登陆百度账号，复制 API Key 和 Secret Key，填写到软件中方可使用。
+保存 key 以后将不会出现此弹窗。
+''')
+        webbrowser.open_new("https://wenxin.baidu.com/younger/apiDetail?id=20006")
+        return None
+
+def get_Secret_Key():
+    if os.path.exists('key.json'):
+        f = open('key.json', 'r')
+        content = f.read()
+        key = json.loads(content)
+        return key['Secret Key']
+    else:
+        return None
+
+# 创建控制台元素
+# Key 获取 API Key 和 Secret Key
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="API Key", label_info="API Key", default=get_API_Key()),
+    BaseButton(bind_func=yanggu_callback, text="旸谷社区")],
+    text="点击右方按钮进入旸谷社区，网站最下方调用专区点击“申请调用”。登陆百度账号，即可获取API Key Secret Key。"))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="Secret Key", label_info="Secret Key", default=get_Secret_Key()),
+    BaseButton(bind_func=key_save, text="保存key")],
+    text="为避免每次打开软件都需要重复输入key，点击右方按钮可以保存key，明文存储在同级目录 key.json，谨防泄露。"))
+q_gui.add_notebook_tool(BaseButton(bind_func=wechatpay_callback, text="赞赏作者"))
+
+# 续写
+q_gui.add_notebook_tool(InputBox(name="续写输入框", label_info="请输入文本", default="烟把黑夜烫了一个洞，",tab_index = 1))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [],
+    tab_index=1,
+    text="字数输入为整数，介于1到1000之间，字数越多所需时间越久，同时可能出现重复内容。"))
+q_gui.add_notebook_tool(InputBox(name="续写输入框最多字数", label_info="最多字数", default="128",tab_index = 1))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="续写输入框最少字数", label_info="最少字数", default="1"),
+     BaseButton(bind_func=click_run_custom)],
+     tab_index = 1))
+# 病句
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="病句输入框", label_info="请输入病句", default="我们提倡节约用水,反对豪无节制地用水,浪废水资源地行为。"),
+    BaseButton(bind_func=click_run_correction)],
+    tab_index = 2,
+    text = "框内输入病句，点击开始执行，控制台会返回改错后的句子。"))
+# 作文
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="作文输入框", label_info="请输入题目", default="理想")],
+    tab_index = 3,
+    text = "框内输入作文的标题，点击开始执行，控制台会返回生成的作文。"))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [RadioButton(["记叙文", "议论文", "说明文", "应用文", "读后感","日记"])],
+    tab_index = 3,
+    text = "请选择所需的作文体裁。"))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="作文输入框最多字数", label_info="最多字数", default="256")],
+    tab_index=3,
+    text="字数输入为整数，介于1到1000之间，字数越多所需时间越久。"))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="作文输入框最少字数", label_info="最少字数", default="100"),
+     BaseButton(bind_func=click_run_zuowen)],
+     tab_index = 3))
+# 摘要
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="摘要输入框", label_info="请输入文本", default='''19号，印度一些主流媒体发布消息称，
+汉语普通话被批准成为巴基斯坦官方语言！消息称，巴基斯坦参议院19号通过将汉语普通话作为官方语言的议案，如果普通话成为巴基斯坦官方语言，
+中巴关系会进一步深化，两国人民在中巴经济走廊建设中的沟通也会变得更简单。到底是不是真消息呢？据记者了解，事实上，
+该决议只是提到鼓励学习中国官方语言，并没有提到汉语普通话要成为巴基斯坦的官方语言。'''),
+    BaseButton(bind_func=click_run_summarization)],
+    tab_index = 4,
+    text = "框内输入需要生成摘要的文本，点击开始执行，控制台会返回生成的摘要。"))
+# 古诗
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="古诗输入框", label_info="请输入文本", default="小小黄花尔许愁。楚事悠悠。")],
+    tab_index = 5,
+    text = "框内输入古诗的开头，点击开始执行，控制台会返回生成的古诗续写内容。若生成结果不理想可尝试在文本后加逗号。"))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [],
+    tab_index=5,
+    text="字数输入为整数，介于1到1000之间，字数越多所需时间越久，同时可能出现非古诗正文内容。"))
+q_gui.add_notebook_tool(InputBox(name="古诗输入框最多字数", label_info="最多字数", default="64",tab_index = 5))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="古诗输入框最少字数", label_info="最少字数", default="1"),
+     BaseButton(bind_func=click_run_poetry)],
+     tab_index = 5))
+# 对联
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="对联输入框", label_info="请输入上联", default="风云三尺剑"),
+    BaseButton(bind_func=click_run_couplet)],
+    tab_index = 6,
+    text = "框内输入上联，点击开始执行，控制台会返回生成的下联。"))
+# 改写
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="改写输入框", label_info="请输入文本", default="蓝色性格的人如何还击别人？"),
+    BaseButton(bind_func=click_run_rewrite)],
+    tab_index = 7,
+    text = "框内输入需要改写的文本，点击开始执行，控制台会返回生成的意思相近的改写结果，论文狗降重利器！"))
+# 词语
+q_gui.add_notebook_tool(InputBox(name="词语输入框", label_info="请输入词语", default="还击",tab_index = 8))
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [CheckButton(options=[("近义词", 1), ("反义词", 1), ("释义", 1)]),
+    BaseButton(bind_func=click_run_words)],
+    tab_index = 8,
+    text = "需要该成语的 近义词/反义词/释义 在相应的框前打对勾"))
+# 押韵
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="押韵输入框", label_info="请输入一个字", default="花"),
+    BaseButton(bind_func=click_run_rhyme)],
+    tab_index = 9,
+    text = "框内输入需要押韵的字，点击开始执行，控制台会返回与这个字押韵的字。"))
+# 组词
+q_gui.add_notebook_tool(HorizontalToolsCombine(
+    [InputBox(name="组词输入框", label_info="请输入一个字", default="华"),
+    BaseButton(bind_func=click_run_rhyme_words)],
+    tab_index = 10,
+    text = "框内输入需要组词的字，点击开始执行，控制台会返回含这个字的词语"))
+
+
+q_gui.run()
---
+++ README.md
@@ -0,0 +1,92 @@
+# AI写作外挂
+
+## 软件截图
+
+![软件截图](软件截图.png)
+
+## 方案背景
+
+服务于所有与文字打交道的人。
+
+中小学生常常受到写作文不知道从何下手的困扰，此软件可以提供作文生成。或者写到一半灵感枯竭，凑不够字数，此软件可以续写。学习过程中难免出现病句，此软件提供病句改错功能。
+
+新闻工作者常常需要提取摘要，此软件可以提供摘要生成参考。
+
+古诗对联爱好者，需要大量的灵感，此软件提供妙手偶得之的途径。
+
+写论文最让人头疼的就是查重，同义改写为论文降重提供可能。
+
+文章由句子组成，句子由字词组成，提供字词，供写作者使用。
+
+## 运行方式
+
+### 编程环境
+
+python 版本 3.9.1
+
+qgui 版本 0.6.2
+
+### 安装依赖
+
+```
+pip install qgui
+```
+
+其他用到的库：requests，json，webbrowser，os
+
+### 注意事项
+
+`AI_writing_helper.py` 文件第 19 行，请仔细阅读。为了软件需求更改了一点点 `qgui` 的文件，更改后的文件也已经上传，可以选择做相同修改，也可以选择注释掉作者添加的部分，不影响软件功能。
+
+### 运行源码
+
+直接运行 `AI_writing_helper.py` 即可。
+
+## 打包软件
+
+### 编程环境
+
+pyinstaller 版本 5.2
+
+### 安装依赖
+
+```
+pip install pyinstaller
+```
+
+### 注意事项
+
+直接打包，运行打包后的软件出现类似以下问题：
+
+![直接打包软件报错](直接打包软件报错.png)
+
+找不到 `double_down.png`，给的是一个很长的temp路径不存在。（原问题错误没有截屏，和这个错误就最后一行不一样）
+
+问题出在 `tkinter` 找不到图片，我的解决方案是，将 `double_down.png` 和 `double_up.png` 两个图片的路径改为软件的相对路径，软件同级目录加一个 `images` 文件夹。
+
+同样需要 `qgui` 本身的代码，在 `qgui\third_party\collapsing_frame.py` 文件第 27 到 30 行，改为如下：
+
+```python
+        self.images = [tkinter.PhotoImage(name='open',
+                                          file="images/double_down.png"),
+                       tkinter.PhotoImage(name='closed',
+                                          file="images/double_up.png")]
+```
+
+可以手动更改，可以直接用我放在 `qgui\third_party\collapsing_frame.py` 的文件。
+
+如果不想更改的话，不影响源码的执行，打包时候可能报错。
+
+### 打包命令
+
+```
+pyinstaller -F -w -i AI写作外挂.ico AI_writing_helper.py --collect-all ttkbootstrap
+```
+
+### 软件发行
+
+将 `AI_writing_helper.exe` 更名为 `AI写作外挂.exe`
+
+再将 `AI写作外挂.exe` 与 `images` 文件夹一同压缩为 `AI写作外挂.zip`
+
+软件运行时必须保证，`AI写作外挂.exe` 与`images` 文件夹同级，不可单独移动其一。
